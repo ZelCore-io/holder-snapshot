47003505941db75b2dd82c91448b5a7fd41c3a86
stultusmundi <bmg.stultusmundi@gmail.com>
2021-10-14 18:09:10 +0300

    Minor fix in creating export folder

--- index.js
+++ index.js
@@ -2,10 +2,19 @@
  * @author StultusMundi
  */
 
+const path = require('path');
+const fs = require('fs');
+
 const solana = require('./solana');
 const tron = require('./tron');
 
-// change below
+// Create export folder if not exists
+const homeDirPath = path.join(__dirname, './export');
+if (!fs.existsSync(homeDirPath)) {
+  fs.mkdirSync(homeDirPath);
+}
+
+// change below for correct contracts
 const solanaMint = 'dK83wTVypEpa1pqiBbHY3MNuUnT3ADUZM4wk9VZXZEc'; // Wrapped Aave testing CHANGE
 const tronContract = 'TMjQiyqCxfhvxJbrZN9amk9u6n5nEAAgpa'; // KDA testing CHANGE
 
