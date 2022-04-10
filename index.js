88c150eeb5b0ec0d0a9911f440f0fbde01330dd9
Stultus Mundi <bmg.stultusmundi@gmail.com>
2022-04-10 22:44:02 +0300

    flux-sol flux-tron contracts

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ if (!fs.existsSync(homeDirPath)) {
 }
 
 // change below for correct contracts
-const solanaMint = 'dK83wTVypEpa1pqiBbHY3MNuUnT3ADUZM4wk9VZXZEc'; // Wrapped Aave testing CHANGE
-const tronContract = 'TMjQiyqCxfhvxJbrZN9amk9u6n5nEAAgpa'; // KDA testing CHANGE
+const solanaMint = 'FLUX1wa2GmbtSB6ZGi2pTNbVCw3zEeKnaPCkPtFXxqXe'; // Flux-sol
+const tronContract = 'TWr6yzukRwZ53HDe3bzcC8RCTbiKa4Zzb6'; // Flux-tron
 const ethContract = '0x720cd16b011b987da3518fbf38c3071d4f0d1495';
 const bscContract = '0xaff9084f2374585879e8b434c399e29e80cce635';
 
