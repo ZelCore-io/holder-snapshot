23ed0a4a5e0a14933363b646b866fd47aac5bacf
Stultus Mundi <bmg.stultusmundi@gmail.com>
2022-04-11 17:28:20 +0300

    Remove content

--- index.js
+++ index.js
@@ -27,10 +27,3 @@ solana.start(solanaMint);
 tron.start(tronContract);
 eth.start(ethContract);
 bsc.start(bscContract);
-
-/**
- * @TODO these 3 addresses in eth snapshot have their balances not calculated correctly
- * 0x65cfe40cae0147f88f66cda30d442a7cfa696f6c
- * 0x10dc5f382693f51cbabb77954689284689ee4c25
- * 0x014ca76e7c467b06b4c908c5802ae45552bd12e1
- */
